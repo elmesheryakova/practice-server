2a3d444ce00a4fd34584512b105c62dbc16189cd
elmesheryakova <mesheryakova-elena90@gmail.com>
2023-04-04 22:25:04 +0300

    add tsc

--- dist/index.js
+++ dist/index.js
@@ -3,5 +3,5 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = require("./app");
 const port = process.env.PORT || 8081;
 app_1.app.listen(port, () => {
-    console.log(`port ${port}`);
+    console.log(`listen port ${port}`);
 });
