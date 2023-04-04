6b06e0de8f6da5d6760cffb09dfc651158457bdc
elmesheryakova <mesheryakova-elena90@gmail.com>
2023-04-04 19:27:19 +0300

    ts to js

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = require("./app");
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 8081;
 app_1.app.listen(port, () => {
     console.log(`port ${port}`);
 });
