138eeca54c12cb376abcf4cc5e037ccbeffb3789
elmesheryakova <mesheryakova-elena90@gmail.com>
2023-04-06 21:59:44 +0300

    add deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,11 +10,13 @@ git reset --hard origin/master
 
 echo "Install app dependencies"
 sudo rm -rf node_modules package-lock.json
+sudo apt-get install npm
 sudo npm install
 
 echo "Build your app"
 sudo npm run tsc
 
 echo "Run new PM2 action"
+npm install pm2 -g
 sudo cp /home/apps/ecosystem.json ecosystem.json
 sudo pm2 start ecosystem.json
