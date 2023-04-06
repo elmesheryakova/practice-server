d4ca7b8febf8b53d5f5b4b628e5419a02b71cb4f
elmesheryakova <mesheryakova-elena90@gmail.com>
2023-04-06 23:24:24 +0300

    deploy

--- dist/db/db.js
+++ dist/db/db.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.db = void 0;
 exports.db = {
     users: [
-        { id: 1, name: 'Yurok' },
+        { id: 1, name: 'Yura' },
         { id: 2, name: 'Vitya' },
         { id: 3, name: 'Masha' },
         { id: 4, name: 'Vadim' }
