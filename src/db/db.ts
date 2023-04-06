3ca4ad364fb00a565b0d71e4b4a0c8feb83dbe53
elmesheryakova <mesheryakova-elena90@gmail.com>
2023-04-06 23:19:51 +0300

    deploy

--- src/db/db.ts
+++ src/db/db.ts
@@ -4,7 +4,7 @@ export type UsersType = {
 }
 export const db: DBType = {
   users: [
-    {id: 1, name: 'Yurik'},
+    {id: 1, name: 'Yurok'},
     {id: 2, name: 'Vitya'},
     {id: 3, name: 'Masha'},
     {id: 4, name: 'Vadim'}
