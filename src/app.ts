6a042d6c19267b4d555a3eb8025c25ca3aa04ef0
elmesheryakova <mesheryakova.elena90@gmail.com>
2024-01-21 17:46:21 +0300

    cors

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,27 @@
 import express from "express";
-import { db } from "./db/db";
-import { getUsersRouter } from "./Routes/users";
-import cors from "cors"; // Добавьте эту строку
+import {db} from "./db/db";
+import {getUsersRouter} from "./Routes/users";
+import bodyParser from "body-parser";
+import cors from 'cors'
+// import jwt from 'jsonwebtoken'
+export const app = express()
+export const jsonBodyMiddleware = express.json()
 
-export const app = express();
-export const jsonBodyMiddleware = express.json();
+app.use(jsonBodyMiddleware)
 
-app.use(jsonBodyMiddleware);
-app.use(cors()); // Добавьте эту строку
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // Здесь можно указать конкретный домен фронтенда вместо '*'
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-})
-app.use('/users', getUsersRouter(db));
+app.use('/users', getUsersRouter(db))
+app.use(bodyParser.json(), cors())
+//
+// app.post('/login', (req, res) => {
+//   const token = jwt.sign({user: 'user name', admin: true}, 'PRACTICE_KEY', {expiresIn: '5d'})
+//   try {
+//     if(!(req.body.email === 'test@gmail.com' && req.body.password === '123456')) {
+//       console.log('this user not found')
+//     }
+//     res.status(200).send({accessToken: token})
+//   } catch (e) {
+//     res.status(400).send('User not found')
+//     console.log(e)
+//   }
+// })
+
