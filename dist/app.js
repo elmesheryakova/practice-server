93744a65b97ae34bab159da5f6d75eefc63c5760
elmesheryakova <mesheryakova.elena90@gmail.com>
2024-01-21 17:56:05 +0300

    cors

--- dist/app.js
+++ dist/app.js
@@ -7,24 +7,14 @@ exports.jsonBodyMiddleware = exports.app = void 0;
 const express_1 = __importDefault(require("express"));
 const db_1 = require("./db/db");
 const users_1 = require("./Routes/users");
-const body_parser_1 = __importDefault(require("body-parser"));
 const cors_1 = __importDefault(require("cors"));
-// import jwt from 'jsonwebtoken'
 exports.app = (0, express_1.default)();
 exports.jsonBodyMiddleware = express_1.default.json();
 exports.app.use(exports.jsonBodyMiddleware);
+const corsOptions = {
+    origin: ["http://localhost", "http://practice-m.ru"],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+};
+exports.app.use((0, cors_1.default)(corsOptions));
 exports.app.use('/users', (0, users_1.getUsersRouter)(db_1.db));
-exports.app.use(body_parser_1.default.json(), (0, cors_1.default)());
-//
-// app.post('/login', (req, res) => {
-//   const token = jwt.sign({user: 'user name', admin: true}, 'PRACTICE_KEY', {expiresIn: '5d'})
-//   try {
-//     if(!(req.body.email === 'test@gmail.com' && req.body.password === '123456')) {
-//       console.log('this user not found')
-//     }
-//     res.status(200).send({accessToken: token})
-//   } catch (e) {
-//     res.status(400).send('User not found')
-//     console.log(e)
-//   }
-// })
--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,20 @@
 import express from "express";
-import {db} from "./db/db";
-import {getUsersRouter} from "./Routes/users";
-import bodyParser from "body-parser";
-import cors from 'cors'
-// import jwt from 'jsonwebtoken'
-export const app = express()
-export const jsonBodyMiddleware = express.json()
+import { db } from "./db/db";
+import { getUsersRouter } from "./Routes/users";
+import cors from "cors";
 
-app.use(jsonBodyMiddleware)
+export const app = express();
+export const jsonBodyMiddleware = express.json();
 
-app.use('/users', getUsersRouter(db))
-app.use(bodyParser.json(), cors())
-//
-// app.post('/login', (req, res) => {
-//   const token = jwt.sign({user: 'user name', admin: true}, 'PRACTICE_KEY', {expiresIn: '5d'})
-//   try {
-//     if(!(req.body.email === 'test@gmail.com' && req.body.password === '123456')) {
-//       console.log('this user not found')
-//     }
-//     res.status(200).send({accessToken: token})
-//   } catch (e) {
-//     res.status(400).send('User not found')
-//     console.log(e)
-//   }
-// })
+app.use(jsonBodyMiddleware);
 
+const corsOptions = {
+    origin: ["http://localhost", "http://practice-m.ru"],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+
+app.use('/users', getUsersRouter(db));
