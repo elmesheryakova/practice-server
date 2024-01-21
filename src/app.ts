2545223e37873144a57e5c0763b6bf8faa726f31
elmesheryakova <mesheryakova.elena90@gmail.com>
2024-01-21 17:43:23 +0300

    cors

--- src/app.ts
+++ src/app.ts
@@ -8,5 +8,10 @@ export const jsonBodyMiddleware = express.json();
 
 app.use(jsonBodyMiddleware);
 app.use(cors()); // Добавьте эту строку
-
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Здесь можно указать конкретный домен фронтенда вместо '*'
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+})
 app.use('/users', getUsersRouter(db));
