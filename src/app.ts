ea535ad48c0a1343716658911aa231a12f0a375a
elmesheryakova <mesheryakova.elena90@gmail.com>
2024-01-24 19:28:49 +0300

    cors

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
 import express from "express";
 import { db } from "./db/db";
 import { getUsersRouter } from "./Routes/users";
-import cors from "cors";
+import bodyParser from "body-parser";
+import cors from 'cors';
 
 export const app = express();
 export const jsonBodyMiddleware = express.json();
 
 app.use(jsonBodyMiddleware);
-
-app.use(cors({origin: /.*/}))
-
+// app.use(cors({origin: /.*/}))
+app.use(bodyParser.json(), cors())
 
 app.use('/users', getUsersRouter(db));
